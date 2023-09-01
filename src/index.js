030dab4240bfa43286fa2f092a25104021987b8f
amalvarghese593 <amalvarghese593@gmail.com>
2023-09-01 08:09:53 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const app = require("./app");
 
 dotenv.config();
 let server;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+// const PORT = process.env.PORT || 5000;
 
 const connectToDB = async () => {
   try {
